094e7a24c4bff2eee9ea4709f88c52b75bce2dd7
Freddy-Rios <freddyrios2000@gmail.com>
2023-12-06 10:03:43 -0700

    added button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,22 @@ import Image from "next/image";
 const data = { Germany: "Berlin", azerbaijan: "Baku" };
 
 function ButtonGame({ data }: { data: Record<string, string> }) {
-  return <>hello world? part two</>;
+  // grab data into countries and capitals
+  const countries = Object.keys(data);
+  const capitals = Object.values(data);
+
+  // combine arrays for options to select randomly
+  const options = [...countries, ...capitals];
+
+  return (
+    <>
+      {options.map((option) => (
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          {option}
+        </button>
+      ))}
+    </>
+  );
 }
 
 export default function Home() {
@@ -12,4 +27,10 @@ export default function Home() {
       <ButtonGame data={data} />
     </>
   );
+}
+{
+  /* <a href="#_" class="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+<span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+<span class="relative">Button Text</span>
+</a> */
 }
